00b17b6c5b53d3e29492af157c41b78b6f8b1152
cjdelisle <calebdelisle@lavabit.com>
2011-06-13 07:11:20 -0400

    Added a ByteConverter which converts arrays of any type for which there is a converter already. The overhead is 1 byte per array entry for array entries of length less than 254 bytes. For entries longer than 254 bytes the overhead is 3 bytes unless the entry is longer than 65534 bytes in which case the overhead is 7 bytes. Null entries are preserved and the converter differentiates between an array which is null and an empty array.

---
+++ src/main/java/com/spidertracks/datanucleus/convert/ArrayConverter.java
@@ -0,0 +1,253 @@
+/**********************************************************************
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors :
+    ...
+ ***********************************************************************/
+package com.spidertracks.datanucleus.convert;
+
+import java.lang.reflect.Array;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.scale7.cassandra.pelops.ColumnFamilyManager;
+
+/**
+ * A ByteConverter which can convert an array of anything for which there is an exiting converter.
+ * This converter places a 1, 3, or 6 byte value behind each element in the array to indicate the length
+ * of the next element.
+ *
+ * A null value for an array corrisponds to no content written.
+ * An empty array corrisponds to a single 0xFF byte.
+ * Everything else is represented as:
+ * length|value|length|value|length|value
+ * The length of a length entry is variable but the end point can be determined by reading it.
+ * see: {@link writeLength(int, ByteBuffer)} for more information about length encoding.
+ * A null entry will corrispond to a single 0xFE byte.
+ *
+ * @version $Id$
+ * @since 1.1.1-0.7.0
+ */
+public class ArrayConverter implements ByteConverter
+{
+    /** The converter for the type of object which this converter handles an array of. */
+    private final ByteConverter innerConverter;
+
+    /**
+     * An empty array which is the proper type for this array converter.
+     * This is the type of array returned by {@link #getObject(ByteBuffer, ByteConverterContext)}.
+     */
+    private final Object[] arrayInstance;
+
+    /**
+     * The Constructor.
+     *
+     * @param innerConverter the converter for the type of object which this converter handles an array of.
+     * @param innerType the class of object which this converter handles an array of.
+     */
+    public ArrayConverter(final ByteConverter innerConverter, final Class<?> innerType)
+    {
+        this.innerConverter = innerConverter;
+        this.arrayInstance = (Object[]) Array.newInstance(innerType, 0);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see ByteConverter#getObject(ByteBuffer, ByteConverterContext)
+     */
+    @Override
+    public Object getObject(final ByteBuffer buffer, final ByteConverterContext context)
+    {
+        // No content corrisponds to null.
+        if (buffer == null || buffer.remaining() == 0) {
+            return null;
+        }
+
+        // Only one byte, if it is 0xFF then this is an empty array,
+        // a single 0xFF byte is an invalid length.
+        if (buffer.remaining() == 1) {
+            buffer.mark();
+            if ((buffer.get() & 0xFF) == 0xFF) {
+                return this.arrayInstance;
+            }
+            buffer.reset();
+        }
+
+        List<Object> out = new ArrayList<Object>();
+        int originalLimit = buffer.limit();
+        int next = buffer.position();
+        for (;;) {
+            if (originalLimit == next) {
+                // Reached the limit of the buffer, we're done.
+                break;
+            }
+
+            int length = readLength(buffer);
+
+            if (length < 0) {
+                // -1 length indicates a null entry.
+                out.add(null);
+                next++;
+                continue;
+            }
+
+            next = buffer.position() + length;
+            try {
+                buffer.limit(next);
+            } catch (IllegalArgumentException e) {
+                throw new RuntimeException("Could not parse input for entry #" + (out.size() + 1) + " in "
+                                           + "array. (failed to reset buffer limit from " + buffer.limit()
+                                           + " to " + next + " this probably means the content was "
+                                           + "altered.\nThe first " + out.size() + " entries were " + out);
+            }
+
+            out.add(this.innerConverter.getObject(buffer, context));
+
+            buffer.limit(originalLimit);
+            buffer.position(next);
+        }
+
+        return out.toArray(this.arrayInstance);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see ByteConverter#writeBytes(Object, ByteBuffer, ByteConverterContext)
+     */
+    @Override
+    public ByteBuffer writeBytes(final Object value,
+                                 final ByteBuffer buffer,
+                                 final ByteConverterContext context)
+    {
+        if (value == null) {
+            return buffer;
+        }
+
+        final Object[] values = (Object[]) value;
+
+        if (values.length == 0) {
+            return ConverterUtils.check(buffer, 1).put((byte) 0xFF);
+        }
+
+        ByteBuffer tmpBuff = ByteBuffer.allocate(65535);
+        ByteBuffer out = buffer;
+
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] == null) {
+                out = ConverterUtils.check(out, 1);
+                writeLength(-1, out);
+                continue;
+            }
+
+            // Need to mark the temp buffer because check() uses reset().
+            tmpBuff.mark();
+
+            tmpBuff = this.innerConverter.writeBytes(values[i], tmpBuff, context);
+            int length = tmpBuff.position();
+
+            // Check 7 bytes + length because 7 is the
+            // greatest number of bytes it can take to represent a length.
+            out = ConverterUtils.check(out, length + 7);
+
+            writeLength(length, out);
+
+            // Copy the content from the temp buffer to the out buffer.
+            tmpBuff.flip();
+            out.put(tmpBuff);
+            tmpBuff.clear();
+        }
+
+        return out;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see ByteConverter#getComparatorType()
+     */
+    @Override
+    public String getComparatorType()
+    {
+        return ColumnFamilyManager.CFDEF_COMPARATOR_BYTES;
+    }
+
+    /* --------------------- Stateless. --------------------- */
+
+    /**
+     * Write the length in a space efficient way.
+     * For lengths less than 255 bytes long, one byte is used.
+     * For lengths between 255 and 65534, 3 bytes are used.
+     * For lengths between 65534 and 2,147,483,648 (Integer.MAX_VALUE), 7 bytes are used.
+     * The encoding scheme for entries less than 254 bytes long is:
+     * <length (1 byte)> content
+     * For less than 65535:
+     * 0xFF <length (2 bytes)> content
+     * For longer:
+     * 0xFF 0xFF 0xFF <length (4 bytes)> content
+     * If length is less than 0 (used to indicate a null entry), a single 0xFE byte is stored.
+     *
+     * @param length the length of the content which will be written next.
+     * @param writeTo the buffer to write the encoded length to.
+     */
+    private static void writeLength(final int length, final ByteBuffer writeTo)
+    {
+        if (length < 0) {
+            writeTo.put((byte) 0xFE);
+            return;
+        }
+
+        if (length < 254) {
+            writeTo.put((byte) length);
+            return;
+        }
+
+        writeTo.put((byte) 0xFF);
+
+        if (length < 65535) {
+            writeTo.putShort((short) length);
+            return;
+        }
+        writeTo.putShort((short) 0xFFFF).putInt(length);
+    }
+
+    /**
+     * Read a length of a piece of data as generated by {@link #writeLength(int, ByteBuffer)}.
+     * If the length input to writeLength was less than 0, -1 is returned.
+     *
+     * @param readFrom the ByteBuffer to read the data from.
+     * @return the unpacked length.
+     */
+    private static int readLength(final ByteBuffer readFrom)
+    {
+        // using bitwise and to prevent signedness of the byte.
+        int value = readFrom.get() & 0xFF;
+
+        if (value == 0xFE) {
+            return -1;
+        }
+
+        if (value != 0xFF) {
+            return value;
+        }
+
+        value = readFrom.getShort() & 0xFFFF;
+        if (value != 0xFFFF) {
+            return value;
+        }
+
+        return readFrom.getInt();
+    }
+}
---
+++ src/test/java/com/spidertracks/datanucleus/convert/ArrayConverterTest.java
@@ -0,0 +1,151 @@
+/**********************************************************************
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors :
+    ...
+ ***********************************************************************/
+package com.spidertracks.datanucleus.convert;
+
+import org.junit.Assert;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+import org.scale7.cassandra.pelops.Bytes;
+import org.scale7.cassandra.pelops.ColumnFamilyManager;
+
+/**
+ * Run tests against {@link ArrayConverter}
+ *
+ * @version $Id$
+ * @since 1.1.1-0.7.0
+ */
+public class ArrayConverterTest
+{
+    /**
+     * Test method for {@link ArrayConverter#getObject(org.scale7.cassandra.pelops.Bytes)}.
+     * Happy path test for getObject.
+     */
+    @Test
+    public void testGetObject()
+    {
+        final byte[] stored = {
+            (byte)5, (byte)'H', (byte)'e', (byte)'l', (byte)'l', (byte)'o',
+            (byte)6, (byte)'W', (byte)'o', (byte)'r', (byte)'l', (byte)'d', (byte)'!',
+            (byte)2, (byte)':', (byte)')', (byte)0, (byte)0xFE
+        };
+
+        final String[] expected = {"Hello", "World!", ":)", "", null};
+
+        final ArrayConverter arrayOfStringConverter =
+            new ArrayConverter(new StringConverter(), String.class);
+
+        final ByteBuffer buffer = ByteBuffer.wrap(stored);
+        final String[] returned = (String[]) arrayOfStringConverter.getObject(buffer, null);
+
+        Assert.assertEquals(5, returned.length);
+        Assert.assertEquals("Hello", returned[0]);
+        Assert.assertEquals("World!", returned[1]);
+        Assert.assertEquals(":)", returned[2]);
+
+        // This does not work because StringConverter confuses null with ""
+        //Assert.assertEquals("", returned[3]);
+
+        Assert.assertNull(returned[4]);
+    }
+
+    /**
+     * Prove that the converter knows the difference between an empty array and null.
+     */
+    @Test
+    public void testGetEmptyAndNull()
+    {
+        final ByteBuffer empty = ByteBuffer.wrap(new byte[] { (byte)0xFF });
+
+        final ArrayConverter arrayOfStringConverter =
+            new ArrayConverter(new StringConverter(), String.class);
+
+        Assert.assertNull(arrayOfStringConverter.getObject(null, null));
+        Assert.assertNull(arrayOfStringConverter.getObject(ByteBuffer.wrap(new byte[0]), null));
+        Assert.assertEquals(0, ((Object[]) arrayOfStringConverter.getObject(empty, null)).length);
+    }
+
+    /**
+     * Put an array and then get it back.
+     */
+    @Test
+    public void testPutAndGet() throws Exception
+    {
+        final String[] control = new String(
+            "Governments of the Industrial World, you weary giants of flesh and steel, I come from "
+          + "Cyberspace, the new home of Mind. On behalf of the future, I ask you of the past to "
+          + "leave us alone. You are not welcome among us. "
+          + "You have no sovereignty where we gather.").split(" ");
+
+        final ArrayConverter arrayOfStringConverter =
+            new ArrayConverter(new StringConverter(), String.class);
+
+        final ByteBuffer bb = arrayOfStringConverter.writeBytes(control, null, null);
+
+        bb.flip();
+        final String[] test = (String[]) arrayOfStringConverter.getObject(bb, null);
+
+        Assert.assertEquals(control.length, test.length);
+        for (int i = 0; i < control.length; i++) {
+            Assert.assertEquals(control[i], test[i]);
+        }
+    }
+
+    /**
+     * Put an array with large elements and then get it back.
+     */
+    @Test
+    public void testPutAndGetLongEntry() throws Exception
+    {
+        final String[] control = { "Hello", null, "World!", null, ":)", null, "...", "" };
+
+        final StringBuilder sb1 = new StringBuilder(254);
+        final StringBuilder sb2 = new StringBuilder(255);
+        final StringBuilder sb3 = new StringBuilder(65536);
+
+        for (int i = 0; i < 254; i++) {
+            sb1.append('1');
+        }
+        for (int i = 0; i < 255; i++) {
+            sb2.append('2');
+        }
+        for (int i = 0; i < 65536; i++) {
+            sb3.append('3');
+        }
+
+        control[1] = sb1.toString();
+        control[3] = sb2.toString();
+        control[5] = sb3.toString();
+
+        final ArrayConverter arrayOfStringConverter =
+            new ArrayConverter(new StringConverter(), String.class);
+
+        final ByteBuffer bb = arrayOfStringConverter.writeBytes(control, null, null);
+
+        bb.flip();
+        final String[] test = (String[]) arrayOfStringConverter.getObject(bb, null);
+
+        Assert.assertEquals(control.length, test.length);
+        // Can't test last entry since StringConverter makes it in to null.
+        for (int i = 0; i < control.length - 1; i++) {
+            Assert.assertEquals(control[i], test[i]);
+        }
+        Assert.assertNull(test[test.length - 1]);
+    }
+}
